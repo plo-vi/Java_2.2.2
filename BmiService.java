34246354791ddec1f7c167f082b4118317766615
viktoriia287 <75584040+viktoriia287@users.noreply.github.com>
2021-01-23 20:11:18 +0300

    Update BmiService.java

--- BmiService.java
+++ BmiService.java
@@ -2,15 +2,10 @@ public class BmiService {
 
     public double calculate(double weight, double height) {
 
-        if(weight < 0) {
+        if(weight < 0 || height < 0) {
             weight = 0;
         }
-
-//        if(height < 0) {
-//            height = 0;
-//        }
 
         return weight / height*height;
     }
 }
-
